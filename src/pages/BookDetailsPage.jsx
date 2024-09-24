cb3037a1b9ab6d1ebaaa03f53813f46f23c886d1
shakil12 <tahmidul.work@gmail.com>
2024-09-25 00:02:54 +0600

    add to card and wish to read buttons functionality added

--- src/pages/BookDetailsPage.jsx
+++ src/pages/BookDetailsPage.jsx
@@ -1,5 +1,6 @@
 import { useParams } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
+import Swal from 'sweetalert2';
 
 const BookDetailsPage = () => {
   const { id } = useParams();
@@ -20,6 +21,25 @@ const BookDetailsPage = () => {
     return <div>Book not found!</div>;
   }
 
+  const handleWishToRead = () => {
+    Swal.fire({
+      title: 'Added to Wishlist!',
+      text: `${book.bookId} ${book.bookName} has been successfully added to the Wishlist`,
+      icon: 'success',
+      confirmButtonText: 'OK',
+    });
+  };
+  
+  const handleAddToCart = () => {
+    Swal.fire({
+      title: 'Added to Cart!',
+      text: `${book.bookId} ${book.bookName} has been successfully added to the Cart`,
+      icon: 'success',
+      confirmButtonText: 'OK',
+    });
+  };
+  
+
   return (
     <div className="container mx-auto p-8 rounded-lg">
       <div className="flex flex-col lg:flex-row bg-white rounded-lg shadow-md overflow-hidden">
@@ -33,10 +53,10 @@ const BookDetailsPage = () => {
           <p className="text-base sm:text-lg mb-2"><strong>Publisher:</strong> {book.publisher}</p>
           <p className="text-base sm:text-lg mb-4"><strong>Year:</strong> {book.yearOfPublishing}</p>
           <div className="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-4">
-            <button className="mt-4 px-6 py-3 bg-green-500 text-white rounded hover:bg-green-600 transition duration-300">
+            <button onClick={handleWishToRead} className="mt-4 px-6 py-3 bg-green-500 text-white rounded hover:bg-green-600 transition duration-300">
               Wish to Read
             </button>
-            <button className="mt-4 px-6 py-3 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-300">
+            <button onClick={handleAddToCart} className="mt-4 px-6 py-3 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-300">
               Add to Cart
             </button>
           </div>
