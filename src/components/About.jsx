fef253e8841d2c56b37d95ce113c320806b5b024
shakil12 <tahmidul.work@gmail.com>
2024-09-25 09:18:05 +0600

    about and blog margin added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 const About = () => {
     return (
-      <div className="container mx-auto my-8">
+      <div className="container mx-auto my-8 p-12">
         <h1 className="text-3xl font-bold">About</h1>
         <div>
           <h3 className="mt-4 text-xl font-bold">This is about page</h3>
--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,6 +1,6 @@
 const Blog = () => {
     return (
-      <div className="container mx-auto my-8">
+      <div className="container mx-auto my-8 p-12">
         <h1 className="text-3xl font-bold">Blog</h1>
         <div>
           <h3 className="mt-4 text-xl font-bold">This is blog page</h3>
